691e924dcabd333834c35f385245202c4c1539d1
Isaac <ingisaacbr97@gmail.com>
2025-08-26 16:41:39 -0600

    Ejercicio Better Calculator 26/08/2025_Isaac Barrios Rodríguez

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-
+/*
 export function addTwoNumbers(x, y){
   console.log(x+y)
   return x+y
@@ -16,4 +16,55 @@ console.log (r3)
 console.log (r4)
 }
 
-main()
+main()*/
+
+function sumar (x,y){
+  
+  return x+y;
+}
+
+function restar(x,y){
+  return x-y;
+}
+
+function multiplicar(x,y){
+  return x*y;
+}
+
+function dividir(x,y){
+  return x/y;
+}
+
+
+
+const option= +prompt ("Hola! Selecciona una de las siguientes opciones de tu calculadora:\n 1.Sumar\n 2.Restar\n 3.Multiplicar\n 4.Dividir")
+switch (option){
+  
+  case 1:
+    const num1= +prompt("Ingresa un número: ")
+    const num2= +prompt("Ingresa otro número: ")
+    console.log(sumar(num1,num2));
+  break;
+
+  case 2:
+    const num3= +prompt("Ingresa un número: ")
+    const num4= +prompt("Ingresa otro número: ")
+    console.log(restar(num3,num4));
+  break;
+  
+  case 3:
+    const num5= +prompt("Ingresa un número: ")
+    const num6= +prompt("Ingresa otro número: ")
+    console.log(multiplicar(num5,num6));
+  break;
+  
+  case 4:
+    const num7= +prompt("Ingresa un número: ")
+    const num8= +prompt("Ingresa otro número: ")
+    console.log(dividir(num7,num8));
+  break;  
+
+  default:
+    console.log("Escoje una opción valida");
+    
+}
