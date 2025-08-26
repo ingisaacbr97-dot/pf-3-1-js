9ae1a35c32ef8f1eda1bc095d2c4276dc03a9c5a
Isaac <ingisaacbr97@gmail.com>
2025-08-26 10:49:49 -0600

    Ejercicio resuelto

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 
-export function addTwoNumbers(/*aqui tambien va código*/){
-  //aqui va tu código
+export function addTwoNumbers(x, y){
+  console.log(x+y)
+  return x+y
 }
+
+let r1= addTwoNumbers(2,3)
+let r2= addTwoNumbers(-2,-3)
+let r3= addTwoNumbers(13,26)
+let r4= addTwoNumbers (-10,30)
+
+function main(){
+console.log (r1)
+console.log (r2)
+console.log (r3)
+console.log (r4)
+}
+
+main()
